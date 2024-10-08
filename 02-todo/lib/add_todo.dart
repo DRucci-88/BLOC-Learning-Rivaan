7728f51c4fe55aa984be2fcd4b3ea51a7cedae85
DRucci-88 <d.rucci.2001@gmail.com>
2024-10-08 13:24:45 +0700

    02 Ui Only

---
+++ 02-todo/lib/add_todo.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class AddTodo extends StatefulWidget {
+  const AddTodo({super.key});
+
+  @override
+  State<AddTodo> createState() => _AddTodoState();
+}
+
+class _AddTodoState extends State<AddTodo> {
+  final _todoTitleController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Add Todo'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            TextField(
+              controller: _todoTitleController,
+              decoration: const InputDecoration(hintText: 'Title'),
+            ),
+            const SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Add'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
